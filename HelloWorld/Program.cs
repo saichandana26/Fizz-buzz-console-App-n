c7ff28d7b49ec4c02f8df82a82dcf4e67895d8f1
saichandana.pagadala <saichandana.pagadala@outlook.com>
2022-12-16 17:32:14 +0530

    Renamed file name

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -6,17 +6,7 @@ namespace HelloWorld
     {
         static void Main(string[] args)
         {
-            /*
-            Console.WriteLine("What is your name?");
-            var name = Console.ReadLine();
-            var currentDate = DateTime.Now;
-            Console.WriteLine($"{Environment.NewLine}Hello, {name}, " +
-                $"on {currentDate:d} at {currentDate:t}!");
-            Console.Write($"{Environment.NewLine}Press any key to exit...");
-            Console.ReadKey(true);
-            */
-
-
+        
             Class1 c = new Class1();
             var currentDate = DateTime.Now;
             Console.WriteLine($"Today is {currentDate.DayOfWeek}");
