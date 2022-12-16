3386caefe303bd5ad1689db35d4ec7afd7536c13
saichandana.pagadala <saichandana.pagadala@outlook.com>
2022-12-16 17:18:43 +0530

    Fizz buzz console application

---
+++ HelloWorld/Class1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorld
+{
+    internal class Class1
+    {
+       
+        public void display(string a, string b)
+        {
+            Console.WriteLine("Enter number:");
+            int n = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Number entered is :" + n);
+            Console.WriteLine($"{Environment.NewLine}");
+            for (int i = 1; i < n; i++)
+            {
+                if (i % 3 == 0 & i % 5 == 0) { Console.WriteLine(a+b); continue; }
+                if (i % 3 == 0) { Console.WriteLine(a); continue; }
+                if (i % 5 == 0) { Console.WriteLine(b); continue; }
+                Console.WriteLine(i);
+            }
+        }
+
+    }
+}
--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -1,9 +1,12 @@
-﻿namespace HelloWorld
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace HelloWorld
 {
     internal class Program
     {
         static void Main(string[] args)
         {
+            /*
             Console.WriteLine("What is your name?");
             var name = Console.ReadLine();
             var currentDate = DateTime.Now;
@@ -11,6 +14,26 @@
                 $"on {currentDate:d} at {currentDate:t}!");
             Console.Write($"{Environment.NewLine}Press any key to exit...");
             Console.ReadKey(true);
+            */
+
+
+            Class1 c = new Class1();
+            var currentDate = DateTime.Now;
+            Console.WriteLine($"Today is {currentDate.DayOfWeek}");
+
+            if (Convert.ToString(currentDate.DayOfWeek) == "Wednesday")
+            {
+                c.display("wizz", "wuzz");
+            }
+            else
+            { 
+                c.display("fizz", "buzz");
+            }
+            
+
         }
+       
     }
+
+
 }
